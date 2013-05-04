fcfc96bfe90d9df3ddc506c601ba350e27b11451
Owner <Owner@192.168.137.117>
2013-05-04 13:46:35 -0400

    Parametrized the floor. -Dave

--- src/mygame/Environment.java
+++ src/mygame/Environment.java
@@ -18,19 +18,21 @@ public class Environment {
     private static InteractiveObject testBox[], testSphere[];
     private static Inert floor;
     private static Inert tiles[];
+    private static final float tileDim = 1.5f;
+    private static final int floorExtent = 7;//must be an odd number
     
     public static void create(){
-        int n = 5;
-        testBox = new TestBox[n*n];
-        testSphere = new InteractiveObject[n*n];
-        tiles = new Inert[n*n];
-        float xOffset = 2.5f*n/2f;
-        float zOffset = 2.5f*n/2f;
-        for(int i=0;i<n*n;i++){
+
+        testBox = new TestBox[floorExtent*floorExtent];
+        testSphere = new InteractiveObject[floorExtent*floorExtent];
+        tiles = new Inert[floorExtent*floorExtent];
+        float xOffset = tileDim*floorExtent/2f;
+        float zOffset = tileDim*floorExtent/2f;
+        for(int i=0;i<floorExtent*floorExtent;i++){
             //testBox[i] = new TestBox( new Vector3f((i/n)*0.7f-2f,1f,(i%n)*0.7f+1), new Vector3f(0.2f, 0.2f, 0.2f));
             //testSphere[i] = new TestBox( new Vector3f((i/n)*3,3,(i%n)*3), new Vector3f(0.1f, 0.1f, 0.1f));//new InteractiveObject(new Geometry("Lulz",new Sphere(10, 10, 1f)), new Vector3f((i/n)*3,3,(i%n)*3));
             ColorRGBA color = (i%2)>0 ? ColorRGBA.White : ColorRGBA.LightGray;
-            tiles[i] = new Inert(new Geometry("Lulz",new Box(2.5f,0.1f,2.5f)) , new Vector3f(5f*(i%n)-xOffset,-1.25f,2.5f+i/5*5f-zOffset),color);
+            tiles[i] = new Inert(new Geometry("Lulz",new Box(tileDim,0.1f,tileDim)) , new Vector3f(2f*tileDim*(i%floorExtent)-xOffset,-1.25f,2.5f+i/floorExtent*tileDim*2f-zOffset),color);
         }
         //floor = new Inert(new Geometry("Lulz",new Box(2.5f,0.1f,2.5f)) , new Vector3f(0,-1.25f,2.5f));
     
